eab282b4c136b4c0309cb445855f1c8849d8fb8e
Tamiris Cristine <44439100+TamirisCss@users.noreply.github.com>
2022-12-02 12:16:30 +0100

    feat: add theme colors file

---
+++ src/theme/dark.js
@@ -0,0 +1,16 @@
+const darkTheme = {
+  title: "dark",
+  isPositive: "#1db489",
+  notPositive: "#dc414c",
+
+  colors: {
+    bg: "#1e202a",
+    bgTop: "#1f212e",
+    bgCard: "#252a41",
+    text: "#8b97c6",
+    textTheme: "#ffffff",
+    toggle: "#3eda82",
+  },
+};
+
+export default darkTheme;
---
+++ src/theme/light.js
@@ -0,0 +1,14 @@
+const lightTheme = {
+  title: "light",
+
+  colors: {
+    bg: "#ffffff",
+    bgTop: "#f5f7ff",
+    bgCard: "#f0f2fa",
+    text: "#63687e",
+    textTheme: "#1e202a",
+    toggle: "#aeb3cb",
+  },
+};
+
+export default lightTheme;
